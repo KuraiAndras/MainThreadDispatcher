771aff203a9d784587ef429acc6f90a8fde4f02a
Kurai András <k.andras9642@gmail.com>
2020-03-18 18:41:42 +0100

    Add dispatcher interface

---
+++ MainThreadDispatcher/IMainThreadDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MainThreadDispatcher
+{
+    public interface IMainThreadDispatcher
+    {
+        void Invoke(Action action);
+        Task InvokeAsync(Action action);
+        Task<T> InvokeAsync<T>(Func<T> func);
+    }
+}
