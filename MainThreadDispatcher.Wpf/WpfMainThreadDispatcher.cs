6bf28fe086f85b12ee99d7d889132381d25e3616
Kurai András <k.andras9642@gmail.com>
2020-04-22 17:27:37 +0200

    Add wpf project

---
+++ MainThreadDispatcher.Wpf/WpfMainThreadDispatcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MainThreadDispatcher.Wpf
+{
+    public sealed class WpfMainThreadDispatcher : IMainThreadDispatcher
+    {
+        private readonly int _mainThreadId;
+        private readonly Dispatcher _dispatcher;
+
+        public WpfMainThreadDispatcher()
+        {
+            _mainThreadId = Thread.CurrentThread.ManagedThreadId;
+            _dispatcher = Application.Current.Dispatcher;
+        }
+
+        public void Invoke(Action action)
+        {
+            if (action is null) throw new ArgumentNullException(nameof(action));
+
+            if (Thread.CurrentThread.ManagedThreadId == _mainThreadId)
+            {
+                action();
+
+                return;
+            }
+
+            _dispatcher.Invoke(action);
+        }
+
+        public async Task InvokeAsync(Action action)
+        {
+            if (action is null) throw new ArgumentNullException(nameof(action));
+
+            if (Thread.CurrentThread.ManagedThreadId == _mainThreadId)
+            {
+                action();
+
+                return;
+            }
+
+            await _dispatcher.InvokeAsync(action);
+        }
+
+        public async Task<T> InvokeAsync<T>(Func<T> func)
+        {
+            if (func is null) throw new ArgumentNullException(nameof(func));
+
+            if (Thread.CurrentThread.ManagedThreadId == _mainThreadId)
+            {
+                return func();
+            }
+
+            return await _dispatcher.InvokeAsync(func);
+        }
+    }
+}
